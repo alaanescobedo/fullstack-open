c28ff0bcad111b8e97b7911ac3cfeee8dce4d67b
Alan Escobedo <alaanprogramacion@gmail.com>
2022-02-25 11:16:35 -0600

    Add 4.6: helper functions and unit tests, step4 - mostBlogs function

--- part-04/bloglist/utils/list_helper.js
+++ part-04/bloglist/utils/list_helper.js
@@ -1,4 +1,6 @@
 const totalLikes = (listBlogs) => {
+  if (listBlogs.length === 0) return null
+
   const allLikes = listBlogs.reduce((acc, current) => {
     return acc + current.likes
   }, 0)
@@ -8,12 +10,31 @@ const favoriteBlog = (listBlogs) => {
   if (listBlogs.length === 0) return null
 
   const favorite = listBlogs.reduce((acc, current) => {
-    return acc.likes > current.likes ? acc : current
+    return current.likes > acc.likes ? current : acc
   }, { likes: 0 })
   return favorite
 }
 
+const mostBlogs = (listBlogs) => {
+  if (listBlogs.length === 0) return null
+
+  const totalBlogsByAuthor = listBlogs.reduce((acc, current) => {
+    acc[current.author] = acc[current.author] ? acc[current.author] + 1 : 1
+    return acc
+  }, {})
+
+  const listOfTuples = Object.entries(totalBlogsByAuthor)
+
+  const result = listOfTuples.reduce((acc, current) => {
+    const blogsByAuthor = { author: current[0], blogs: current[1] }
+    return blogsByAuthor.blogs > acc.blogs ? blogsByAuthor : acc
+  }, { author: '', blogs: 0 })
+
+  return result
+}
+
 module.exports = {
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
